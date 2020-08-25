a2c7c7ac7cd6b6e8dffc04f31a121fe5ee7efe45
nathsagar96 <nathsagar96@gmail.com>
2020-08-25 17:17:08 +0530

    Styled ‘Header’ (Inline Styling)

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Header from './Header';
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <button>Add</button>
       <div>
         <span>Name: </span>
-        <br/>
+        <br />
         <span>Phone: </span>
-        </div> 
+      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 function Header() {
+  const headerStyle = {
+    textAlign: 'center',
+    padding: 20,
+    background: '#000',
+    color: '#fff',
+    textTransform: 'uppercase'
+  };
+
   return (
-  <div className="header">
-    Phone Directory
-  </div>
+    <div className="header" style={headerStyle}>
+      Phone Directory
+    </div>
   );
 }
 
