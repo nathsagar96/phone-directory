5b23985bd3355b177b8efbc12c06915df48509bf
nathsagar96 <nathsagar96@gmail.com>
2020-08-26 12:36:20 +0530

    Passed heading to Header component as props

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Header heading="phone directory" />
       <div className="component-body-container">
         <button className="custom-btn add-btn">Add</button>
 
@@ -31,7 +31,7 @@ function App() {
           subscribers.map(
             (subscriber) => {
               return (
-                <div className="grid-container">
+                <div key={subscriber.id} className="grid-container">
                   <div className="grid-item">{subscriber.name}</div>
                   <div className="grid-item">{subscriber.phone}</div>
                   <span className="grid-item action-btn-container">
